a7708d6e69be3e8e2c1158e07e1045d8933a3d27
narek-arsh <narekikol@gmail.com>
2026-06-29 18:51:05 +0200

    Update DATABASE_URL retrieval and format

--- app/database.py
+++ app/database.py
@@ -6,7 +6,10 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL", "").replace(
+    "postgresql://", "postgresql+pg8000://"
+)
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
